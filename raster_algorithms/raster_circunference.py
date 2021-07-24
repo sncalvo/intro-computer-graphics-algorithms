18ad582bd3887fb141a8739016d98327a2628423
Santiago Calvo <sncalvo@hotmail.com>
2021-07-24 20:05:17 -0300

    Fix circle drawing

--- raster_algorithms/raster_circunference.py
+++ raster_algorithms/raster_circunference.py
@@ -1,33 +1,36 @@
 from image import create_canvas
 from collections import namedtuple
 from raster_line import RED, Point
+from math import ceil, sqrt
 from PIL import Image
 
-def alg_1(r: int, center: Point):
-    new_d = 0
-    data = create_canvas()
-    y = center.y + r
-    data[center.x, y] = RED
+def draw_symetric_point(center: Point, offset: Point, data):
+    x, y = offset
+    data[center.y - y, center.x + x] = RED
+    data[center.y + y, center.x + x] = RED
+    data[center.y + y, center.x - x] = RED
+    data[center.y - y, center.x - x] = RED
+    data[center.y - x, center.x + y] = RED
+    data[center.y + x, center.x + y] = RED
+    data[center.y - x, center.x - y] = RED
+    data[center.y + x, center.x - y] = RED
 
-    for x in range(center.x, center.x + r):
+def alg_1(r: int, center: Point):
+    new_d = 5/4 - r
+    data = create_canvas()
+    y = r
+
+    for x in range(0, ceil(r / sqrt(2))):
+        draw_symetric_point(center, Point(x, y), data)
         if new_d < 0:
-            y -= 1
             new_d += 2*x + 3
         else:
-            new_d += 2*x - 2*y  + 5
-
-        data[x, y] = RED
-        data[x, -y] = RED
-        data[-x, -y] = RED
-        data[-x, y] = RED
-        data[y, x] = RED
-        data[y, -x] = RED
-        data[-y, x] = RED
-        data[-y, -x] = RED
+            y -= 1
+            new_d += 2*x - 2*y + 5
 
     img = Image.fromarray(data)
     img.save("circunference/temp3.png")
 
 if __name__ == '__main__':
     import timeit
-    print(timeit.timeit("alg_1(128, Point(512, 512))", globals=locals(), number=1))
+    print(timeit.timeit("alg_1(64, Point(128, 128))", globals=locals(), number=1))
